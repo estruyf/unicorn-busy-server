92a55dc69634e01ba108994adda66f83f0a16772
Elio Struyf <info@estruyf.be>
2020-04-05 13:02:52 +0200

    Added service script

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
+# Install the required dependencies
 sudo apt-get install python3-pip python3-dev
 sudo pip3 install -r ./requirements.txt
 
+# Change permissions of the start up script
 sudo chmod +x ./start.sh
+
+# Copy service
+sudo cp busylight.service /etc/systemd/system/busylight.service
